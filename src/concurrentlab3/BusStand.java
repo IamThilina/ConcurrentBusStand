00b0281fba726eeda13ecccad678d013a16ca8fa
Iamthilina <333thilina@gmail.com>
2016-09-23 15:26:36 +0530

    Complete Working Solution. (Need To Debug)

--- src/concurrentlab3/BusStand.java
+++ src/concurrentlab3/BusStand.java
@@ -8,12 +8,14 @@ package concurrentlab3;
 import java.util.Observable;
 import java.util.Observer;
 import java.util.concurrent.Semaphore;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author thilina
  */
-public class BusStand implements Observer{
+public class BusStand extends Observable implements Observer{
  
     public static boolean busArrived = false; //toggle this when a bus arrives and departs. this is a lock
     
@@ -23,6 +25,24 @@ public class BusStand implements Observer{
     public void update(Observable o, Object arg) {
         System.out.println("Bus-" + arg.toString() + " Arrived @ Bus Stand");
         busArrived = true;
-        busSemaphore = new Semaphore(50); // initialize a new semaphore at each bus arrival
+        busSemaphore = new Semaphore(5); // initialize a new semaphore at each bus arrival
+        setChanged();
+        notifyObservers(arg); //notifying passengers
     }
+    
+    synchronized public boolean boardBus(){
+        if(!busArrived) // bus has departed
+            return false;
+        try {
+            busSemaphore.acquire();  // passenger gets in the bus
+            if(busSemaphore.availablePermits() == 0){ // bus is fully loaded
+               // departure the bus now
+               busArrived = false;
+            }
+            return true;
+        } catch (InterruptedException ex) {
+            Logger.getLogger(BusStand.class.getName()).log(Level.SEVERE, null, ex);
+            return false;
+        }
+    } 
 }
